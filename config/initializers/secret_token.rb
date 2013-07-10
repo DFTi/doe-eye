3e718ba31e6231c85058865c6eddc75165197cce
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-10 22:53:12 +0200

    started the rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DoeEye::Application.config.secret_key_base = '1d6a9e69962a51611b7769a902d15266c934fccfd30c821517c36b38919f288ac6fce4e021a49081819c19eca8fafab9727816fa375b73ced97ef02ec7fdd6f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DoeEye::Application.config.session_store :cookie_store, key: '_doe-eye_session'
