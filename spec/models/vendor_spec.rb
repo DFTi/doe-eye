7cc3e3297647b3a93bdcd3aa41996d374e3df915
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-11 20:51:33 +0200

    added timestamps to vendors

--- spec/models/vendor_spec.rb
+++ spec/models/vendor_spec.rb
@@ -1,7 +1,11 @@
 require 'spec_helper'
 
 describe Vendor do
+	# validations
 	it { should validate_presence_of(:name) }
 	it { should validate_presence_of(:api_key) }
 	it { should validate_presence_of(:api_password) }
+
+	# fields
+	it { should be_timestamped_document }
 end
