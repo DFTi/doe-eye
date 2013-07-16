2f970b92b368f8b762ddbe160ffa5d1fac7bea94
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-16 05:38:06 +0200

    testing tabs

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -1,12 +1,12 @@
 class Vendor
-	include Mongoid::Document
-	include Mongoid::Timestamps
-		
-	field :name
-	field :api_key
-	field :api_password
-	
-	embeds_many :authorizations
-
-	validates_presence_of :name, :api_key, :api_password
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  field :name
+  field :api_key
+  field :api_password
+  
+  embeds_many :authorizations
+  
+  validates_presence_of :name, :api_key, :api_password
 end
