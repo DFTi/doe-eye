62b1bb253ae419b4e51d9ccd494085d40b6e3b7c
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-21 20:01:59 +0200

    changed Document spec to create document before fetching

--- spec/requests/api/document_spec.rb
+++ spec/requests/api/document_spec.rb
@@ -10,22 +10,21 @@ describe API do
   end
 
   describe Document do
+    before(:each) do
+      @document = @project.documents.create(title: "My Document")
+    end
+              
     describe "GET /api/v1/projects/:project_id/documents" do
       it 'gets documents for a project' do
         get "/api/v1/projects/#{@project.id}/documents", access_token: @access_token
         response.status.should == 200
-        JSON.parse(response.body).should == []
+        JSON.parse(response.body).should_not eq [] 
       end
     end
     
     describe "GET /api/v1/documents/:id" do
-      before(:each) do
-        @document = @project.documents.create(title: "My Document")
-      end
-
       it 'gets a document' do
         get "/api/v1/documents/#{@document.id}", access_token: @access_token
-        
         response.status.should == 200
         JSON.parse(response.body).should_not eq []
       end
