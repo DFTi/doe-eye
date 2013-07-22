f53a351f435658b7ea33b0e921c7c6b70b2d3de1
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-22 07:53:28 +0200

    removed stupid specs

--- spec/features/vendor_management_spec.rb
+++ spec/features/vendor_management_spec.rb
@@ -2,11 +2,14 @@ require 'spec_helper'
 
 feature 'Vendor management' do
 	scenario 'User creates a new vendor' do
+    pending
 	end
 
 	scenario 'User updates a vendor' do
+    pending
 	end
 	
 	scenario 'User deletes a vendor' do
+    pending
 	end
 end
