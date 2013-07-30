9f88a00fe0e17a58b743463efbf468e6577a605a
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-07-30 21:11:30 +0200

    refactored vendor management spec

--- spec/features/vendor_management_spec.rb
+++ spec/features/vendor_management_spec.rb
@@ -1,16 +1,20 @@
 require 'spec_helper'
 
 feature 'Vendor management' do
-	scenario 'User creates a new vendor' do
+  let (:vendor_name) { 'test_vendor' }
+  let (:vendor_api_key) { 'test_api_key' }
+  let (:vendor_api_password) { 'test_api_password' }
+
+  scenario 'User creates a new vendor' do
     visit new_vendor_path
     
-    fill_in 'vendor_name', with: 'test'
-    fill_in 'vendor_api_key', with: 'test_api_key'
-    fill_in 'vendor_api_password', with: 'test_api_password'
+    fill_in 'vendor_name', with: vendor_name
+    fill_in 'vendor_api_key', with: vendor_api_key
+    fill_in 'vendor_api_password', with: vendor_api_password
     
     click_button 'Submit'
 
-    page.should have_content 'test'
+    page.should have_content vendor_name
 	end
 
 	scenario 'User updates a vendor' do
