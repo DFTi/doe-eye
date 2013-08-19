a895debcf9ba9e1018ac75f1fdd4ff7ca869ab92
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-08-19 23:00:11 +0200

    stubbed out project management feature specs

---
+++ spec/features/project_management_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Project management' do
+	scenario 'admin creates a project for a vendor' do
+    pending
+	end
+
+	scenario 'admin deletes a project for a vendor' do
+    pending
+	end
+
+	scenario 'admin views all projects for a vendor' do
+    pending
+	end
+end
