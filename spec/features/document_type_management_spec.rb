0fd39b7c7ba00f26eccc4342a1d7cf4e6c6025dc
Jeanre Swanepoel <jeanre@netaddict.co.za>
2013-08-19 22:58:06 +0200

    stubbed out document type management feature specs

---
+++ spec/features/document_type_management_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+feature 'Document Type management' do
+	scenario 'admin creates a new document type' do
+    pending
+	end
+
+	scenario 'admin deletes a document type' do
+    pending
+	end
+
+	scenario 'admin views all document types' do
+    pending
+	end
+end
